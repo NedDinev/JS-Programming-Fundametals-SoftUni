181158c05d331f33d5ad96d091bd2c9adce9af6f
NedDinev <ned.dinev@gmail.com>
2022-06-30 19:15:52 +0300

    New object tasks completed

---
+++ 07. JS-Fundamentals-Objects-and-Classes/01. JS-Fundamentals-Objects-and-Classes-Lab/01.Person-Info.js
@@ -0,0 +1,13 @@
+function personInfo(firstName, lastName, age) {
+  let person = {
+    firstName,
+    lastName,
+    age,
+  };
+
+  person.firstName = firstName;
+  person.lastName = lastName;
+  person.age = age;
+  return person;
+}
+console.log(personInfo("Peter", "Pan", "20"));
---
+++ 07. JS-Fundamentals-Objects-and-Classes/01. JS-Fundamentals-Objects-and-Classes-Lab/02.City.js
@@ -0,0 +1,13 @@
+function city(input) {
+  for (let key of Object.keys(input)) {
+    console.log(`${key} -> ${input[key]}`);
+  }
+}
+
+city({
+  name: "Sofia",
+  area: 492,
+  population: 1238438,
+  country: "Bulgaria",
+  postCode: "1000",
+});
